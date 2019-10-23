266b6f1cf04886e979e13a2aa4ab74428e1243a6
Samuel Santana <samuel.santana@solutis.com.br>
2019-10-22 21:49:48 -0300

    adicionado database

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', () => {});
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Samuel Santana',
+        email: 'samuel@santana',
+        password_hash: 'fafa',
+    });
+    return res.json(user);
+});
 
 export default routes;
